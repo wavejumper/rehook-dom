7b7df28a58c0551ae51dd1cda6a7acab005be1ed
Thomas Crowley <crowley@kibu.com.au>
2019-11-08 09:28:16 +1100

    Fixes..

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wavejumper/rehook-dom "1.1.4"
+(defproject wavejumper/rehook-dom "1.1.5"
   :description "React component DSL for Clojurescript"
   :url "https://github.com/wavejumper/rehook-dom"
 
--- src/rehook/dom/browser.cljs
+++ src/rehook/dom/browser.cljs
@@ -23,7 +23,7 @@
      (react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f)))))
   ([ctx ctx-f props-f e args]
    (let [ctx (ctx-f ctx e)]
-     (react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f)) (props-f args))))
+     (react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f)) (props-f args))))
   ([ctx ctx-f props-f e args & children]
    (let [ctx (ctx-f ctx e)]
-     (apply react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f)) (props-f args) children))))
+     (apply react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f)) (props-f args) children))))
