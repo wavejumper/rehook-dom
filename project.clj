788ea8352dd159fe0b9d6355ddf441edbea92383
Thomas Crowley <crowley@kibu.com.au>
2019-11-16 21:26:32 +1100

    react native fixes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wavejumper/rehook-dom "1.1.6"
+(defproject wavejumper/rehook-dom "1.1.7"
   :description "React component DSL for Clojurescript"
   :url "https://github.com/wavejumper/rehook-dom"
 
--- src/rehook/dom/native.cljs
+++ src/rehook/dom/native.cljs
@@ -24,13 +24,13 @@
      (react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f)))))
   ([ctx ctx-f props-f e args]
    (let [ctx (ctx-f ctx e)]
-     (react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f)) (clj->js args))))
+     (react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f)) (props-f args))))
   ([ctx ctx-f props-f e args & children]
    (let [ctx (ctx-f ctx e)]
-     (apply react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f)) (clj->js args) children))))
+     (apply react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f)) (props-f args) children))))
 
 (defn component-provider
   ([ctx component]
    (component-provider ctx identity clj->js component))
   ([ctx ctx-f props-f component]
-   (constantly #(react/createElement (bootstrap ctx ctx-f props-f component)))))
+   (constantly #(bootstrap ctx ctx-f props-f component))))
