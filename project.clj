2a2337548854c1f010281ad25352735fb7eb9040
Thomas Crowley <crowley@kibu.com.au>
2019-11-17 19:57:36 +1100

    1.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wavejumper/rehook-dom "1.1.7"
+(defproject wavejumper/rehook-dom "1.1.8"
   :description "React component DSL for Clojurescript"
   :url "https://github.com/wavejumper/rehook-dom"
 
