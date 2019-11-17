bbdd0ca54bf08c681449c547f52556596e3e7c2b
Thomas Crowley <crowley@kibu.com.au>
2019-11-17 19:57:25 +1100

    support rehook-test

--- src/rehook/dom/browser.cljs
+++ src/rehook/dom/browser.cljs
@@ -21,9 +21,15 @@
   ([ctx ctx-f props-f e]
    (let [ctx (ctx-f ctx e)]
      (react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f)))))
+
   ([ctx ctx-f props-f e args]
    (let [ctx (ctx-f ctx e)]
-     (react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f)) (props-f args))))
+     (react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f))
+                          (props-f (dissoc args :rehook/id)))))
+
   ([ctx ctx-f props-f e args & children]
    (let [ctx (ctx-f ctx e)]
-     (apply react/createElement (handle-type e ctx (partial bootstrap ctx ctx-f props-f)) (props-f args) children))))
+     (apply react/createElement
+            (handle-type e ctx (partial bootstrap ctx ctx-f props-f))
+            (props-f (dissoc args :rehook/id))
+            children))))
